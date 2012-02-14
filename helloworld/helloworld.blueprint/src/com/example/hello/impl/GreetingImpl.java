991e8475f7fe89179a9a889675de7a59a6cebce6
Neil Bartlett <njbartlett@gmail.com>
2012-02-14 14:44:44 +0000

    [helloworld]: added config admin integration

--- helloworld/helloworld.blueprint/src/com/example/hello/impl/GreetingImpl.java
+++ helloworld/helloworld.blueprint/src/com/example/hello/impl/GreetingImpl.java
@@ -1,12 +1,35 @@
 package com.example.hello.impl;
 
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 import com.example.hello.Greeting;
 
 public class GreetingImpl implements Greeting {
+	
+	private static final String RESOURCE_BASE = "com.example.hello.impl.greetings";
+	
+	private ResourceBundle resources = ResourceBundle.getBundle(RESOURCE_BASE, Locale.ENGLISH, GreetingImpl.class.getClassLoader());
+	
+	public void setLanguage(String language) {
+		System.out.println("-------> Setting language to " + language);
+		try {
+			resources = ResourceBundle.getBundle(RESOURCE_BASE, new Locale(language), GreetingImpl.class.getClassLoader());
+		} catch (MissingResourceException e) {
+			System.err.println("-------> No greeting available for language: " + language);
+		}
+	}
 
 	@Override
 	public void sayHello(String name) {
-		System.out.println("Hello " + name);
+		String message = MessageFormat.format(resources.getString("greeting"), name);
+		System.out.println(message);
+	}
+	
+	public void refresh() {
+		System.out.println("-------> Configuration updated.");
 	}
 
 }
