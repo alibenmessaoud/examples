3d348fa62b2eb3f099831e29b41384c09762325a
Neil Bartlett <njbartlett@gmail.com>
2012-09-06 07:16:48 +0100

    Added system:uninstall command

--- restclient/com.paremus.example.restclient/src/com/paremus/example/restclient/SystemsCommands.java
+++ restclient/com.paremus.example.restclient/src/com/paremus/example/restclient/SystemsCommands.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 		provide = Object.class,
 		properties = {
 				"osgi.command.scope=system",
-				"osgi.command.function=list|show|install"
+				"osgi.command.function=list|show|install|uninstall"
 		})
 public class SystemsCommands {
 	
@@ -56,7 +56,7 @@ public class SystemsCommands {
 	public void show(@Descriptor("System URI of selected system") String systemUri) throws IOException {
 		out.printf("Showing details for system: %s\n", systemUri);
 		ClientResource resource = new ClientResource(BASE_URL + "/" + URLEncoder.encode(systemUri, "UTF-8"));
-		Representation representation = resource.get(MediaType.TEXT_PLAIN);
+		Representation representation = resource.get();
 		
 		StringWriter outputBuffer = new StringWriter();
 		representation.write(outputBuffer);
@@ -64,6 +64,10 @@ public class SystemsCommands {
 		out.println(outputBuffer.toString());
 	}
 	
+	/*
+	 * Install a new system doc with the HTTP POST method. The URL of the new resource is returned
+	 * in the Location header.
+	 */
 	@Descriptor("Install system document")
 	public void install(@Descriptor("Path to system document on local filesystem") String path) throws IOException {
 		File file = new File(path);
@@ -77,6 +81,19 @@ public class SystemsCommands {
 		Response response = resource.getResponse();
 		String location = response.getLocationRef().toString();
 		out.printf("System uploaded to location %s\n", location);
+	}
+	
+	/*
+	 * Uninstall a system doc with the HTTP DELETE method. Note that according to the HTTP specification,
+	 * DELETE is supposed to be idempotent, i.e. can be repeated with the same parameters without causing
+	 * errors. Therefore uninstalling a system URI that does not exist will have no effect.
+	 */
+	@Descriptor("Uninstall system")
+	public void uninstall(@Descriptor("System URI to uninstall") String systemUri) throws IOException {
+		out.printf("Uninstalling system with URI %s\n", systemUri);
+		
+		ClientResource resource = new ClientResource(BASE_URL + "/" + URLEncoder.encode(systemUri, "UTF-8"));
+		resource.delete();
 	}
 	
 }
