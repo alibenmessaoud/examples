f61302d1cb5c1593d65e005104ffc001fddbae7d
Neil Bartlett <njbartlett@gmail.com>
2012-10-20 02:33:38 +0100

    Updated the mongo system doc

--- datanucleus/cnf/export.sh
+++ datanucleus/cnf/export.sh
@@ -1,2 +1,2 @@
-zip -r --quiet demo.zip camelrepo *system.xml load.osh localrepo packager releaserepo repos.properties
+zip -r --quiet demo.zip camelrepo *system.xml load*.osh localrepo packager releaserepo repos.properties
 echo Saved to demo.zip
