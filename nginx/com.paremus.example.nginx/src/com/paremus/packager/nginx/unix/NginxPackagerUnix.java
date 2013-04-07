b9d64f64a74ee3e353586ac3adbf72135cfa9c6a
Gustavo Morozowski <gustavomzw@gmail.com>
2013-04-07 11:33:33 -0300

    add dynamic libraries support

--- nginx/com.paremus.example.nginx/src/com/paremus/packager/nginx/unix/NginxPackagerUnix.java
+++ nginx/com.paremus.example.nginx/src/com/paremus/packager/nginx/unix/NginxPackagerUnix.java
@@ -3,6 +3,7 @@ package com.paremus.packager.nginx.unix;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.bndtools.service.packager.PackageDescriptor;
@@ -17,48 +18,63 @@ import aQute.libg.command.Command;
 
 @Component(properties = "package.type=nginx")
 public class NginxPackagerUnix implements PackageType {
+	
+	private final static String[] sourceFiles = new String[] {
+		"nginx", "mime.types", "lib/libcrypto.0.9.8.dylib", "lib/libpcre.1.dylib",
+		"lib/libssl.0.9.8.dylib", "lib/libSystem.B.dylib", "lib/libz.1.dylib" };
 
 	@Override
 	public PackageDescriptor create(Map<String, Object> properties, File data)
 			throws Exception {
 		NginxProperties config = Configurable.createConfigurable(NginxProperties.class, properties);
-		PackageDescriptor pd = new PackageDescriptor();
-		File nginx = new File(data, "nginx");
-		if (!nginx.isFile()) {
-			IO.copy(getClass().getResource("/data/nginx"), nginx);
-			run("chmod a+x " + nginx.getAbsolutePath());	
+		File[] destFiles = copyFilesToDir(sourceFiles, "/data", data);
+		for (File file : destFiles) {
+			if (!file.getName().equals("mime.types"))
+				run("chmod a+x " + file.getAbsolutePath());
 		}
-		File configDir = new File(data, "config");
-		if (!configDir.isDirectory()) {
-			boolean created = configDir.mkdir();
-			if (!created)
-				throw new Exception("directory creation failed : " + configDir.getAbsolutePath());
-		}
-		File mimeTypes = new File(configDir, "mime.types");
-		if (!mimeTypes.isFile()) {
-			IO.copy(getClass().getResource("/data/mime.types"), mimeTypes);
-		}
-		File nginxConf = new File(configDir, "nginx.conf");
-		IO.copy(new StringReader(generateConfigFile(config)), new FileWriter(nginxConf));
 		File logsDir = new File(data, "logs");
-		if (!logsDir.isDirectory()) {
+		if (!logsDir.exists()) {
 			boolean created = logsDir.mkdir();
 			if (!created)
 				throw new Exception("directory creation failed : " + logsDir.getAbsolutePath());
 		}
+		File nginxConf = new File(data, "nginx.conf");
+		IO.copy(new StringReader(generateConfigFile(config)), new FileWriter(nginxConf));
+		String nginxPath = new File(data, "nginx").getAbsolutePath();
+		String libPath = new File(data, "lib").getAbsolutePath();
+		String confPath = nginxConf.getAbsolutePath();
+		PackageDescriptor pd = new PackageDescriptor();
 		StringBuilder sb = new StringBuilder();
-		sb.append(nginx.getAbsolutePath()).append(" -p " + data.getAbsolutePath());
-		sb.append(" -c " + nginxConf.getAbsolutePath());
+		sb.append("export DYLD_LIBRARY_PATH=").append(libPath).append("\n");
+		sb.append(nginxPath).append(" -p " + data.getAbsolutePath());
+		sb.append(" -c " + confPath);
 		sb.append(" -g \"daemon off;\"");
 		pd.startScript = sb.toString();
 		sb = new StringBuilder();
-		sb.append(nginx.getAbsolutePath()).append(" -s stop");
+		sb.append(nginxPath).append(" -s stop");
 		pd.stopScript = sb.toString();
 		sb = new StringBuilder();
 		sb.append("ps x | grep nginx | grep 'master process'");
 		pd.statusScript = sb.toString();
 		pd.description = "Nginx Packager";
 		return pd;
+	}
+	
+	private File[] copyFilesToDir(String[] fileNames, String sourcePath, File destPath) throws Exception {
+		ArrayList<File> destFiles = new ArrayList<File>();
+		for (String fileName : fileNames) {
+			File destDir = new File(destPath, fileName).getParentFile();
+			if (destDir != null && !destDir.exists()) {
+				boolean created = destDir.mkdir();
+				if (!created)
+					throw new Exception("directory creation failed : " + destDir.getAbsolutePath());
+			}
+			File dest = new File(destPath, fileName);
+			if (!dest.exists())
+				IO.copy(getClass().getResource(sourcePath + "/" + fileName), dest);
+			destFiles.add(dest);
+		}
+		return destFiles.toArray(new File[0]);
 	}
 	
 	private String generateConfigFile(NginxProperties properties) {
