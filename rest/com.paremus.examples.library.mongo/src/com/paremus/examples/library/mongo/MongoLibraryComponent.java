77b5746e5729b7fa088f5083be6890028446b53e
Neil Bartlett <njbartlett@gmail.com>
2013-05-02 17:11:56 +0100

    [rest] Added mongo impl

---
+++ rest/com.paremus.examples.library.mongo/src/com/paremus/examples/library/mongo/MongoLibraryComponent.java
@@ -0,0 +1,69 @@
+package com.paremus.examples.library.mongo;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.bndtools.service.endpoint.Endpoint;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Deactivate;
+import aQute.bnd.annotation.component.Reference;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoURI;
+import com.paremus.examples.api.library.Book;
+import com.paremus.examples.api.library.Library;
+
+@Component(immediate = true)
+public class MongoLibraryComponent implements Library {
+	
+	private MongoURI boundUri;
+	private Mongo mongo;
+	private DB db;
+	
+	@Reference(target = "(uri=mongodb:*)")
+	void setEndpoint(Endpoint endpoint, Map<String, String> endpointProps) {
+		boundUri = new MongoURI(endpointProps.get(Endpoint.URI));
+	}
+	
+	@Activate
+	public void activate() throws Exception {
+		mongo = boundUri.connect();
+		db = mongo.getDB("test");
+		System.out.printf("Connected to MongoDB instance at address %s, database %s.%n", boundUri, db.getName());
+	}
+	
+	@Deactivate
+	public void deactivate() {
+		mongo.close();
+		System.out.println("Disconnected from " + boundUri);
+	}
+
+	public void add(Book book) {
+		DBObject object = new BasicDBObject();
+		object.put("author", book.getAuthor());
+		object.put("title", book.getTitle());
+		
+		DBCollection coll = db.getCollection("books");
+		coll.insert(object);
+	}
+	
+	public List<Book> listBooks() {
+		List<Book> books = new LinkedList<Book>();
+		
+		DBCollection coll = db.getCollection("books");
+		for (DBObject object : coll.find()) {
+			Book book = new Book((String) object.get("author"), (String) object.get("title"));
+			books.add(book);
+		}
+		
+		return books;
+	}
+
+}
---
+++ rest/org.bndtools.rt.packager.mongodb/src/org/bndtools/rt/packager/mongodb/guard/MongoProcessGuard.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Neil Bartlett.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Neil Bartlett - initial API and implementation
+ ******************************************************************************/
+package org.bndtools.rt.packager.mongodb.guard;
+
+
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.bndtools.service.endpoint.Endpoint;
+import org.bndtools.service.mongodb.MongoProperties;
+import org.bndtools.service.packager.ProcessGuard;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.remoteserviceadmin.RemoteConstants;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.ConfigurationPolicy;
+import aQute.bnd.annotation.component.Reference;
+import aQute.bnd.annotation.metatype.Configurable;
+
+@Component(
+		name="org.bndtools.rt.packager.mongodb.guard",
+		properties = { "type=mongodb" },
+		designateFactory = MongoProperties.class,
+		configurationPolicy = ConfigurationPolicy.optional
+		)
+public class MongoProcessGuard implements ProcessGuard {
+	
+	private static final String MONGO_URI_SCHEME = "mongodb";
+
+	private BundleContext context;
+	private Map<String, Object> props;
+	private MongoProperties config;
+	private ServiceRegistration reg;
+
+	@Activate
+	void activate(BundleContext context, Map<String, Object> props) throws Exception {
+		this.context = context;
+		this.props = props;
+		this.config = Configurable.createConfigurable(MongoProperties.class, props);
+	}
+	
+	public Map<String, Object> getProperties() {
+		return props;
+	}
+	
+	// ...
+
+	public synchronized void state(State state) throws Exception {
+		if (state.isAlive()) {
+			if (reg == null) {
+				String localhost = InetAddress.getLocalHost().getHostAddress();
+				URI mongoUri = new URI(MONGO_URI_SCHEME, null, localhost, config.port(), null, null, null);
+				
+				Properties svcProps = new Properties();
+				svcProps.put(Endpoint.URI, mongoUri.toString());
+				svcProps.put(RemoteConstants.SERVICE_EXPORTED_INTERFACES, "*");
+				reg = context.registerService(Endpoint.class.getName(), new Endpoint() {}, svcProps);
+			}
+		} else if (reg != null) {
+			reg.unregister();
+			reg = null;
+		}
+	}
+}
