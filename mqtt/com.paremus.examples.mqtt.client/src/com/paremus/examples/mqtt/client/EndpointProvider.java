c158e1fe0a6b60488585b898ada7335161c8d41f
Neil Bartlett <njbartlett@gmail.com>
2013-05-09 13:14:27 +0100

    [mqtt] removed unused project

--- mqtt/com.paremus.examples.mqtt.client/src/com/paremus/examples/mqtt/client/EndpointProvider.java
+++
@@ -1,9 +0,0 @@
-package com.paremus.examples.mqtt.client;
-
-import org.bndtools.service.endpoint.Endpoint;
-
-import aQute.bnd.annotation.component.Component;
-
-@Component(properties = { "service.exported.interfaces=*", "uri=foobar" })
-public class EndpointProvider implements Endpoint {
-}
