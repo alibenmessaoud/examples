d97661d6acb59ecf8b5b1a09433c4440e4aa768c
Neil Bartlett <njbartlett@gmail.com>
2013-05-09 14:30:52 +0100

    [nginx] Ensure metatype defaults are applied to config

--- nginx/com.paremus.example.nginx/src/com/paremus/packager/nginx/guard/NginxProcessGuard.java
+++ nginx/com.paremus.example.nginx/src/com/paremus/packager/nginx/guard/NginxProcessGuard.java
@@ -1,12 +1,16 @@
 package com.paremus.packager.nginx.guard;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.bndtools.service.packager.ProcessGuard;
-import com.paremus.service.nginx.NginxProperties;
 
 import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.metatype.Configurable;
+
+import com.paremus.service.nginx.NginxProperties;
 
 @Component(designateFactory = NginxProperties.class, properties = { "type=nginx" }, name="com.paremus.example.nginx.guard")
 public class NginxProcessGuard implements ProcessGuard {
@@ -14,8 +18,18 @@ public class NginxProcessGuard implements ProcessGuard {
 	private Map<String, Object> props;
 	
 	@Activate
-	void activate(Map<String, Object> props) {
-		this.props = props;
+	void activate(Map<String, Object> props) throws Exception {
+		NginxProperties config = Configurable.createConfigurable(NginxProperties.class, props);
+		
+		this.props = new HashMap<String, Object>();
+		this.props.putAll(props);
+
+		// apply defaults back into the props (bit of a hack, can we improve this?)
+		for (Method method : NginxProperties.class.getDeclaredMethods()) {
+			String name = method.getName();
+			Object value = method.invoke(config);
+			this.props.put(name, value);
+		}
 	}
 
 	@Override
