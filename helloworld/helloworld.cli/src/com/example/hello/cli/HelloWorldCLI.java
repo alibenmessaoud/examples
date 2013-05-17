c4af405d3d06f68845e32ee37d784b0eee520fdb
timothyjward <timothyjward@apache.org>
2013-05-17 17:13:56 +0100

    Update helloworld.cli to configurably prefer local

--- helloworld/helloworld.cli/src/com/example/hello/cli/HelloWorldCLI.java
+++ helloworld/helloworld.cli/src/com/example/hello/cli/HelloWorldCLI.java
@@ -1,11 +1,18 @@
 package com.example.hello.cli;
 
+import java.util.Dictionary;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.service.cm.ManagedService;
 
 import com.example.hello.Greeting;
 
-public class HelloWorldCLI {
+public class HelloWorldCLI implements ManagedService {
 	
 	static final String SCOPE = "helloworld";
 	static final String[] FUNCTIONS = { "sayHello" };
@@ -22,16 +29,34 @@ public class HelloWorldCLI {
 		greetingTracker = null;
 	}
 	
+	private volatile boolean preferLocal = false;
+	
+	public void updated(Dictionary properties) {
+		
+		if(preferLocal = properties != null && Boolean.parseBoolean(properties.get("prefer.local").toString())) {
+			System.out.println("-------> Prefer local services");
+		} else {
+			System.out.println("-------> Use standard OSGi service selection rules");
+		}
+	}
+	
 	public void sayHello(String[] args) {
 		if (args == null || args.length < 1)
 			throw new IllegalArgumentException("Missing name argument");
-		
-		Greeting greeting = (Greeting) greetingTracker.getService();
+
+		Greeting greeting = (Greeting) (preferLocal ? findLocal() : greetingTracker.getService());
 		if (greeting == null)
 			System.err.println("Greeting service not available");
 		else
 			greeting.sayHello(args[0]);
 	}
 	
-	
+	private Object findLocal() {
+		for(ServiceReference sr : greetingTracker.getServiceReferences()) {
+			if(sr.getProperty("service.imported") == null) {
+				return greetingTracker.getService(sr);
+			}
+		}
+		return greetingTracker.getService();
+	}
 }
--- helloworld/helloworld.cli/src/com/example/hello/cli/HelloWorldCLIActivator.java
+++ helloworld/helloworld.cli/src/com/example/hello/cli/HelloWorldCLIActivator.java
@@ -4,7 +4,9 @@ import java.util.Properties;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ManagedService;
 
 public class HelloWorldCLIActivator implements BundleActivator {
 
@@ -18,8 +20,12 @@ public class HelloWorldCLIActivator implements BundleActivator {
 		Properties props = new Properties();
 		props.put("osgi.command.scope", HelloWorldCLI.SCOPE);
 		props.put("osgi.command.function", HelloWorldCLI.FUNCTIONS);
+		props.put("service.pid", "com.example.hello.cli");
 
-		reg = context.registerService(HelloWorldCLI.class.getName(), cli, props);
+		reg = context.registerService(new String[] {
+				HelloWorldCLI.class.getName(),
+				ManagedService.class.getName()
+		}, cli, props);
 	}
 
 	@Override
