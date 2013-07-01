e34e3ef874cbe8e617a401100bf8caefab4081ee
Richard Nicholson <richard.nicholson@paremus.com>
2013-07-01 15:09:48 +0100

    removed old Gateway example

--- gateway/gateway.impl/src/com/example/gateway/impl/GatewayConfig.scala
+++
@@ -1,8 +0,0 @@
-package com.example.gateway.impl
-
-import aQute.bnd.annotation.metatype.Meta.OCD
-
-@OCD(id="com.example.gateway",factory=true)    
-trait GatewayConfig {
-	def id: String
-}
--- gateway/gateway.pricer/src/com/example/gateway/pricer/PricerConfig.scala
+++
@@ -1,7 +0,0 @@
-package com.example.gateway.pricer
-
-import aQute.bnd.annotation.metatype.Meta.OCD
-
-trait PricerConfig {
-	def `type`: String // TODO this should be an enum but needs support from bnd to understand scala enums
-}
