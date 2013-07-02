4484a7bb0a210f7bb3fa89d75c3a6e4d72795bc7
Neil Bartlett <njbartlett@gmail.com>
2013-07-02 15:02:38 +0100

    [helloworld] Added simple activator-based example

---
+++ helloworld/helloworld.activator/src/com/example/hello/impl/GreetingImpl.java
@@ -0,0 +1,11 @@
+package com.example.hello.impl;
+
+import com.example.hello.Greeting;
+
+public class GreetingImpl implements Greeting {
+
+	public void sayHello(String name) {
+		System.out.println("Hello " + name);
+	}
+
+}
