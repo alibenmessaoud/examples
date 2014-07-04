96cde001574d7d0a8a6d8081ac903e78a5f641c9
tim.ward@paremus.com <timothyjward@apache.org>
2014-07-04 15:23:43 +0100

    Add version 1.0.1 of the equation including a buggy "fix"

--- fractal/com.paremus.demo.fractal.equation/src/com/paremus/demo/fractal/equation/JuliaEquation.java
+++ fractal/com.paremus.demo.fractal.equation/src/com/paremus/demo/fractal/equation/JuliaEquation.java
@@ -5,6 +5,8 @@
 
 package com.paremus.demo.fractal.equation;
 
+import java.util.Random;
+
 import aQute.bnd.annotation.component.Component;
 
 import com.paremus.demo.fractal.api.ComplexNumber;
@@ -63,6 +65,8 @@ public class JuliaEquation implements Equation {
         return values;                             
     }
 
+    private final Random random = new Random();
+    
     /**
      * Convert the number of iterations into a colour index
      * 
@@ -72,6 +76,12 @@ public class JuliaEquation implements Equation {
      * @return
      */
 	private int toColour(int value, int colourDepth, int maxPossible) {
+		
+		//Add a bug by including some noise
+		int noise = (int) ((random.nextGaussian() / 2.0d) * maxPossible);
+		value = value + noise;
+		value = (value < 0) ? 0 : (value > maxPossible) ? maxPossible : value;
+				
 		int i = value * colourDepth / maxPossible -1;
 		return i < 0 ? 0 : i;
 	}
